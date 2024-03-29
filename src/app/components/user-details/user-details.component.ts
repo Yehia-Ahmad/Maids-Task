10076b71857bb5d92c2580866c990a8588c6c506
Yehia <Yehia>
2024-03-29 22:23:43 +0200

    Add card details component

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
+  data$: any;
+  pageNum: number = 1;
+  totalPages: number;
+  loading$ = this.loader.loading$;
 
-  constructor() { }
+  constructor(private api: ApiService, private loader: LoadingService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.getCardDetails(id);
+    });
+  }
+
+  getCardDetails(id: string) {
+    this.api.getElementByID(id).subscribe((res: any) => {
+      this.data$ = res.data;
+    });
+  }
+
+  getPeopleDetails() {
+    return this.router.navigate([`./home`]);
   }
 
 }
